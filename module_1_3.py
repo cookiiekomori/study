b335c13ef7390b1608368f1dbd7dea5261178815
cookiiekomori <cookiekomori@outlook.com>
2024-09-17 18:08:30 +1000

    Update module_1_3.py

--- module_1_3.py
+++ module_1_3.py
@@ -2,7 +2,7 @@ name = 'Danil'
 print('Name:', name)
 age = 24
 print('Age:', age)
-age = 26
+age = age + 1
 print('New age:', age)
-is_student = True
+is_student = 25 > 24
 print('Is Student:', is_student)
