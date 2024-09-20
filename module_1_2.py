2534cccdf304da6d0f77fa04e6d9a835ece12500
cookiiekomori <cookiekomori@outlook.com>
2024-09-21 07:22:04 +1000

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+completed_DZ = 12
+hours_spent = 1.5
+name_course = 'Python'
+average_execution_time = hours_spent / completed_DZ
+# Курс: Python, всего задач:12, затрачено часов: 1.5, среднее время выполнения 0.125 часа.
+print('Курс: ' + name_course + '. Всего задач:', str(completed_DZ) + '. Затрачено часов:', str(hours_spent) + '. Среднее время выполнения:', str(average_execution_time) + ' часа.')
