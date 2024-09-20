b31afec6c4d9dceb7ac4e17daaf8d66374f3614f
cookiiekomori <cookiekomori@outlook.com>
2024-09-21 07:18:40 +1000

    Add files via upload

---
+++ homework_Basic-data-structures.py
@@ -0,0 +1,21 @@
+# 1st program
+print(9 ** 0.5 *5)
+print('----------------------')
+
+# 2nd program 
+print(99.9 > 99.8 and 1000 != 1000.1)
+print('----------------------')
+
+# 3rd program 
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+print('----------------------')
+
+# 4th program 
+string = '123.456'
+number = float(string)
+result = number * 10
+print(int(result) % 10)
+# или же
+print(int((float('123.456')) * 10) % 10)
