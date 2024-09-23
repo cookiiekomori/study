3fffb8b92645582a7add209efc8959597647d56e
cookiiekomori <cookiekomori@outlook.com>
2024-09-23 22:56:02 +1000

    Create module_2_2.py

---
+++ module_2_2.py
@@ -0,0 +1,6 @@
+first = int(input('1. Введите целое число: '))
+second = int(input('2. Введите целое число: '))
+third = int(input('3. Введите целое число:'))
+
+if first == second == third:
+	print('ugadal')
