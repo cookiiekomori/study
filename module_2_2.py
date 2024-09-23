909fe32615bfc35bbd03697c1126c6c4d53a3f0b
cookiiekomori <cookiekomori@outlook.com>
2024-09-23 22:58:36 +1000

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -1,6 +1,10 @@
 first = int(input('1. Введите целое число: '))
 second = int(input('2. Введите целое число: '))
-third = int(input('3. Введите целое число:'))
+third = int(input('3. Введите целое число: '))
 
 if first == second == third:
-	print('ugadal')
+	print('3 числа равны <3')
+elif first == second or first == third or second == third:
+	print('2 числа равны <3')
+else:
+	print('0, все числа не равны (')
