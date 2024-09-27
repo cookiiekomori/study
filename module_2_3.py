eed69d044bf25cfef470cdc5093c6e48bad63390
cookiiekomori <cookiekomori@outlook.com>
2024-09-27 22:19:33 +1000

    Create module_2_3.py

---
+++ module_2_3.py
@@ -0,0 +1,12 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+
+m_list = 0
+while True:
+	a = int(my_list[m_list])
+	if a > 0:
+		print(a)
+	elif a == 0:
+		pass
+	else:
+		break
+	m_list += 1
